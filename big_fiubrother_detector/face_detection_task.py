294a7923020b6b5a2592c08b26708e02a27d4f72
Gabriel <ga-yo-so@hotmail.com>
2019-12-05 23:04:21 -0300

    frame process db row correction

--- big_fiubrother_detector/face_detection_task.py
+++ big_fiubrother_detector/face_detection_task.py
@@ -1,7 +1,8 @@
 from big_fiubrother_core import QueueTask
 from big_fiubrother_core.db import (
     Database,
-    Face
+    Face,
+    FrameProcess
 )
 from big_fiubrother_core.messages import (
     FrameMessage,
@@ -41,22 +42,33 @@ class FaceDetectionTask(QueueTask):
         frame_bytes = face_detection_message.payload
 
         # Convert to cv2 img
-        frame = cv2.imdecode(np.fromstring(frame_bytes, np.uint8), cv2.IMREAD_COLOR)
+        frame = cv2.imdecode(np.frombuffer(frame_bytes, np.uint8), cv2.IMREAD_COLOR)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # Detect faces
-        print("- Performing detection")
+        #print("- Performing detection - frame_id: " + str(frame_id))
+        #rects = []
         rects = self.face_detector.detect_face_image(frame)
 
         if len(rects) > 0:
+            # Create face process record
+            frame_process = FrameProcess(frame_id=frame_id,
+                                         total_faces_count=len(rects))
+            self.db.add(frame_process)
+
             for rect in rects:
+                x1, y1, x2, y2 = rect
+                db_rect = [[x1, y1], [x2, y2]]
+
                 # Insert detected face to db
-                face = Face(frame_id=frame_id, bounding_box=rect)
+                face = Face(frame_id=frame_id, bounding_box=db_rect)
                 self.db.add(face)
 
                 # Get cropped face
-                x1, y1, x2, y2 = rect
                 cropped_face = frame[y1:y2, x1:x2]
+
+                #cv2.imshow("asd", cv2.cvtColor(cropped_face, cv2.COLOR_RGB2BGR))
+                #cv2.waitKey(1)
 
                 # Queue face embedding job
                 face_embedding_message = FaceEmbeddingMessage(video_chunk_id, face.id, cropped_face)
---
+++ testing/sampler_mock_webcam.py
@@ -0,0 +1,95 @@
+import sys
+import yaml
+import cv2
+from queue import Queue
+from big_fiubrother_core.utils import image_to_bytes
+from big_fiubrother_core.messages import FrameMessage
+from big_fiubrother_core import (
+    PublishToRabbitMQ,
+    StoppableThread
+)
+from big_fiubrother_core.db import (
+    Database,
+    VideoChunk,
+    Frame
+)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 0:
+
+        print("--------------------------------")
+        print("This script receives a list of images and sends them to a face detector like a sampler would")
+        print("")
+        print("Usage: ")
+        print("python sampler_mock.py")
+        print("--------------------------------")
+
+    else:
+
+        print('[*] Configuring sampler-mock')
+
+        # input
+        with open("sampler-mock.yml") as config_file:
+            configuration = yaml.safe_load(config_file)
+        print(configuration)
+
+        # Create database connection
+        db = Database(configuration['db'])
+
+        # Create publiser
+        to_publisher_queue = Queue()
+        publisher_thread = StoppableThread(
+                PublishToRabbitMQ(configuration=configuration['publisher'],
+                                  input_queue=to_publisher_queue))
+
+        # Simulate Sampler -----------------------------------------------
+
+        print('[*] Configuration finished. Starting sampler-mock!')
+
+        publisher_thread.start()
+
+        # upload video chunk
+        video_chunk = VideoChunk(camera_id="camera_1",
+                                 timestamp=0.0,
+                                 payload="video_chunk".encode())
+        db.add(video_chunk)
+
+        # sample frames
+        camera = cv2.VideoCapture(0)
+        offset = 0
+        while True:
+
+            # Read frame from Webcam
+            ret, image = camera.read()
+
+            frame = Frame(offset=offset,
+                          video_chunk_id=video_chunk.id)
+            db.add(frame)
+
+            payload = image_to_bytes(image)
+            frame_message = FrameMessage(video_chunk_id=video_chunk.id,
+                                         frame_id=frame.id,
+                                         payload=payload)
+
+            offset += 1
+
+            to_publisher_queue.put(frame_message)
+
+            # Show image on screen
+            cv2.imshow('img', image)
+
+            # Check for exit button 'q'
+            ch = cv2.waitKey(1) & 0xFF
+            if ch == ord("q"):
+                break
+
+        # ----------------------------------------------------------------
+
+        c = input("[*] Wait a few seconds until all frames are sent then press any key to exit")
+
+        publisher_thread.stop()
+        publisher_thread.wait()
+
+        print('[*] sampler-mock stopped!')
